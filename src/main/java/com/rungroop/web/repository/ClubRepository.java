4b7c54bcfa5b09cfa3f9225df9a055561dd167b6
wiktor <nobis171@wp.pl>
2023-12-03 13:22:19 +0100

    api get controller

--- src/main/java/com/rungroop/web/repository/ClubRepository.java
+++ src/main/java/com/rungroop/web/repository/ClubRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ClubRepository extends JpaRepository<Club, Long> {
-    Optional<Club> FindByTitle(String url);
+    Optional<Club> findByTitle(String url);
 }
---
+++ src/main/java/com/rungroop/web/service/impl/ClubServiceImpl.java
@@ -0,0 +1,40 @@
+package com.rungroop.web.service.impl;
+
+import com.rungroop.web.dto.ClubDto;
+import com.rungroop.web.models.Club;
+import com.rungroop.web.repository.ClubRepository;
+import com.rungroop.web.service.ClubService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+
+@Service
+public class ClubServiceImpl implements ClubService {
+
+    private ClubRepository clubRepository;
+    @Autowired
+    public ClubServiceImpl(ClubRepository clubRepository) {
+        this.clubRepository = clubRepository;
+    }
+
+    @Override
+    public List<ClubDto> findAllClubs() {
+        List<Club> clubs = clubRepository.findAll();
+        return clubs.stream().map(this::mapToClubDto).collect(Collectors.toList());
+    }
+
+    private ClubDto mapToClubDto(Club club) {
+        return ClubDto.builder()
+                .id(club.getId())
+                .title(club.getTitle())
+                .photoUrl(club.getPhotoUrl())
+                .content(club.getContent())
+                .createdOn(club.getCreatedOn())
+                .updatedOn(club.getUpdatedOn())
+                .build();
+    }
+}
